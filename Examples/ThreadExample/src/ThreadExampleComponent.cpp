de7a2ef1b42618df970da32822466bf4409ade61
Eduard Münz (pyme04) <emuenz@phoenixcontact.com>
2019-05-17 16:29:30 +0200

    Update to FW2019.3

--- Examples/ThreadExample/src/ThreadExampleComponent.cpp
+++ Examples/ThreadExample/src/ThreadExampleComponent.cpp
@@ -1,66 +1,76 @@
-// Copyright (c) Phoenix Contact GmbH & Co. KG. All rights reserved.  
-// Licensed under the MIT. See LICENSE file in the project root for full license information.  
+/******************************************************************************
+ *
+ * Copyright (c) Phoenix Contact GmbH & Co. KG. All rights reserved.
+ * Licensed under the MIT. See LICENSE file in the project root for full license information.
+ *
+ *  ThreadExampleComponent.cpp
+ *
+ *  Created on: 16.05.2019
+ *  	Author: Eduard Münz, Oliver Warneke
+ *
+ ******************************************************************************/
+
+/******************************************************************************/
+/*  INCLUDES                                                                  */
+/******************************************************************************/
+
 
 #include "ThreadExampleComponent.hpp"
+#include "Arp/Plc/Commons/Esm/ProgramComponentBase.hpp"
 
-namespace ThreadExample 
+namespace ThreadExample
 {
 
 void ThreadExampleComponent::Initialize()
 {
-}
+    // never remove next line
+    ProgramComponentBase::Initialize();
 
-void ThreadExampleComponent::LoadSettings(const String& settingsPath)
-{
-}
-
-void ThreadExampleComponent::SetupSettings()
-{
-}
-
-void ThreadExampleComponent::SubscribeServices()
-{
+    // subscribe events from the event system (Nm) here
 }
 
 void ThreadExampleComponent::LoadConfig()
 {
+    // load project config here
 }
 
 void ThreadExampleComponent::SetupConfig()
 {
-    // DO NOT REMOVE THIS!
-    ThreadExampleComponent::RegisterComponentPorts();
+    // never remove next line
+    ProgramComponentBase::SetupConfig();
+
+    // setup project config here
 }
 
 void ThreadExampleComponent::ResetConfig()
 {
-	//this->dataInfoProvider.Reset();
-}
+    // never remove next line
+    ProgramComponentBase::ResetConfig();
 
-void ThreadExampleComponent::PublishServices()
-{
-}
-
-void ThreadExampleComponent::Dispose()
-{
-}
-
-void ThreadExampleComponent::PowerDown()
-{
+    // implement this inverse to SetupConfig() and LoadConfig()
 }
 
 void ThreadExampleComponent::Start(void) {
-	stopTheThread = false;
+	xStopThread = false;
 	Log::Info("-------------------------------workerThreadInstance start");
 	workerThreadInstance.Start();
 	Log::Info("-------------------------------workerThreadInstance started");
+
+	Log::Info("-------------------------------delegateThreadInstance start");
+	delegateThreadInstance.Start();
+	Log::Info("-------------------------------delegateThreadInstance started");
+
+
+	Log::Info("-------------------------------staticThreadInstance start");
+	staticThreadInstance.Start();
+	Log::Info("-------------------------------staticThreadInstance started");
+
 }
 
 void ThreadExampleComponent::Stop(void) {
 	// if you want to stop some loops of your thread during execution
 	// add something like "stoptheThread" before executing workerThreadStop.
-	stopTheThread = true;
-
+	xStopThread = true;
 	Log::Info("-------------------------------workerThreadInstance stop");
 	workerThreadInstance.Stop();
 	Log::Info("-------------------------------workerThreadInstance stopped");
@@ -69,33 +79,55 @@ void ThreadExampleComponent::Stop(void) {
 /// Thread Body
 void ThreadExampleComponent::workerThreadBody(void) {
 
-	// some time intensive stuff
-	while((x < y) && !stopTheThread)
+	if(xStopThread == false)
 	{
-		x++;
+		if(iCountervalue < iEndValue)
+			iCountervalue ++;
+		else
+			iCountervalue = iStartValue;
+
+		Log::Info("-------------------------------workerThreadInstance is running, iCountervalue={0}", iCountervalue);
 	}
-	Log::Info("-------------------------------workerThreadBody: Count = {0}", (int)x);
+	else
+		Log::Info("-------------------------------workerThreadInstance is stopped, iCountervalue={0}", iCountervalue);
 }
+
+
 
 void ThreadExampleComponent::delegateThreadBody(void* pParameter) {
 	// has access to this*
-	int *i =  reinterpret_cast<int*>(pParameter);
-	while(!stopTheThread)
+	int *i = reinterpret_cast<int*>(pParameter);
+
+	while(!xStopThread)
 	{
-		if(this->myparameter < *i){
-			i++;
-		}
-		Thread::Sleep(5000);
+		if(this->myparameter == *i)
+			Log::Info("-------------------------------ThreadExampleComponent::delegateThreadBody is running successful i={0} , myparameter = {1}", *i, this->myparameter);
+
+		else
+			Log::Info("-------------------------------ThreadExampleComponent::delegateThreadBody is running with ERROR: i={0} , myparameter = {1}", *i, this->myparameter);
+
+		Thread::Sleep(1000);
 	}
+
+	Log::Info("-------------------------------ThreadExampleComponent::delegateThreadBody stopped");
 }
 
 void ThreadExampleComponent::staticThreadBody(void* pParameter) {
 	bool* pValue = (bool*) pParameter;
 	// has NO access to this*
 	while(!*pValue){
+
 		//Do something and sleep for specifiedTime.
-		Thread::Sleep(5000);
+		Log::Info("-------------------------------ThreadExampleComponent::staticThreadBody is running  pParameter={0}", (int) *pValue);
+		Thread::Sleep(500);
 	}
+
+		Log::Info("-------------------------------ThreadExampleComponent::staticThreadBody stopped");
+}
+
+
+int ThreadExampleComponent::GetCounterValue(){
+	return iCountervalue;
 }
 
 } // end of namespace ThreadExample
