06934c13c63eafe67aebd1af94585969220cef2c
Eduard Münz (pyme04) <emuenz@phoenixcontact.com>
2019-05-17 16:32:20 +0200

    Update to FW2019.3

--- Examples/ThreadExample/src/ThreadExampleProgram.cpp
+++ Examples/ThreadExample/src/ThreadExampleProgram.cpp
@@ -1,13 +1,32 @@
+/******************************************************************************
+ *
+ * Copyright (c) Phoenix Contact GmbH & Co. KG. All rights reserved.
+ * Licensed under the MIT. See LICENSE file in the project root for full license information.
+ *
+ *  ThreadExampleProgram.cpp
+ *
+ *  Created on: 16.05.2019
+ *  	Author: Eduard Münz, Oliver Warneke
+ *
+ ******************************************************************************/
+
+/******************************************************************************/
+/*  INCLUDES                                                                  */
+/******************************************************************************/
+
 #include "ThreadExampleProgram.hpp"
 #include "Arp/System/Commons/Logging.h"
 #include "Arp/System/Core/ByteConverter.hpp"
 
-namespace ThreadExample 
+namespace ThreadExample
 {
  
 void ThreadExampleProgram::Execute()
 {
-   //TODO implement program 
+    //implement program 
+
+	i_pCounter = threadExampleComponent.GetCounterValue();
+	Log::Info("-------------------------------Program Cyclic Call:  i_pCounter = {0}", i_pCounter);
 }
 
 } // end of namespace ThreadExample
