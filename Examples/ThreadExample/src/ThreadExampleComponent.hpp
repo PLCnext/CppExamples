395f6852879749f9722703789649309d2789bb88
Oliver Warneke <38212828+OWarneke@users.noreply.github.com>
2021-02-22 09:24:49 +0100

    formatting ThreadExampleComponent.hpp

--- Examples/ThreadExample/src/ThreadExampleComponent.hpp
+++ Examples/ThreadExample/src/ThreadExampleComponent.hpp
@@ -85,7 +85,7 @@ private: // fields
 
     void workerThreadBody(void);
 
-     //Commons/Thread Example
+    //Commons/Thread Example
     Thread delegateThreadInstance;
     Thread staticThreadInstance;
 
